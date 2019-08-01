f3c21996ff279df7bdb3bd7435f45dfdb218fd42
Trey Winward <treydinw@gmail.com>
2019-08-01 16:33:45 -0600

    working on asteroids

--- Projects/astroids/rotatable.py
+++ Projects/astroids/rotatable.py
@@ -1,4 +1,5 @@
 import movable
+import math
 
 class Rotatable(movable.Movable):
 
@@ -11,23 +12,50 @@ class Rotatable(movable.Movable):
     
     
     def rotate(self, delta_rotation):
-        if self.mRotation >= 0 and self.mRotation < 360:
+        self.mRotation += delta_rotation
+        self.mRotation %= 360
+        # if self.mRotation < 0:
+        #     self.mRotation += 360
+        # if self.mRotation >= 360:
+        #     self.mRotation -= 360
+
             
     
     
-    def splitDeltaVIntoXAndY(rotation,delta_velocity)
+    def splitDeltaVIntoXAndY(self, rotation,delta_velocity):
+        dir_x = math.cos(math.radians(rotation)) * delta_velocity
+        dir_y = math.sin(math.radians(rotation)) * delta_velocity
+
+        return(dir_x, dir_y)
+    
+    def accelerate(self, delta_velocity):
+        (dif_x, dif_y) = self.splitDeltaVIntoXAndY(self.mRotation, delta_velocity)
+        self.mDX += dif_x
+        self.mDY += dif_y
     
     
-    def accelerate(delta_velocity)
+    def rotatePoint(self, x, y):
+        d = math.sqrt(x**2 + y**2)
+        angle = math.atan2(y, x)
+        angle_rotation = angle + math.radians(self.mRotation)
+        rotated_x = math.cos(angle_rotation) * d
+        rotated_y = math.sin(angle_rotation) * d
+        return (rotated_x, rotated_y)
+
     
     
-    def rotatePoint(x,y)
+    def translatePoint(self, x,y):
+        return (x+self.mX, y + self.mY)
     
     
-    def translatePoint(x,y)
+    def rotateAndTranslatePoint(self, x,y):
+        new_x, new_y = self.rotatePoint(x,y)
+        new_x, new_y = self.translatePoint(new_x, new_y)
+        return(new_x, new_y)
+
     
-    
-    def rotateAndTranslatePoint(x,y)
-    
-    
-    def rotateAndTranslatePointList(point_list)
+    def rotateAndTranslatePointList(self, point_list):
+        points = []
+        for p in points:
+            points.append(self.rotateAndTranslatePoint(p[0],p[1]))
+        return points
